6fd4b7bcdfda5d23d1ad4e4b49402a242b32d1ef
vira069 <vira.kumar069@gmail.com>
2024-12-13 16:55:47 +0530

    Frontend auto setup files by vite bundler started

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    
+    <>
+      <h1>SparkNest</h1>
+    </>    
+
+  )
+}
+
+export default App
