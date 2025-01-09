2609a885d6a3421c785e507d6b64eb98caffb81f
vira069 <vira.kumar069@gmail.com>
2025-01-10 01:34:33 +0530

    Refactor Login component to enhance layout and styling with responsive design

--- frontend/src/components/Auth/LoginPage.jsx
+++ frontend/src/components/Auth/LoginPage.jsx
@@ -1,11 +1,54 @@
 import React from "react";
 
-const Login = () => {
-    return(
-        <div className='flex items-center justify-center h-screen'>
-      <h1 className='text-3xl font-bold'>Login</h1>
+const LoginPage = () => {
+  return (
+    <div className="flex flex-col md:flex-row h-screen">
+      {/* Left Section */}
+      <div className="w-full md:w-1/2 h-1/2 md:h-full bg-white flex justify-center items-center">
+        <img
+          src="https://plus.unsplash.com/premium_photo-1668736594225-55e292fdd95e?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt="Login Illustration"
+          className="w-full h-full object-cover"
+        />
+      </div>
+      {/* Right Section */}
+      <div className="w-full md:w-1/2 h-1/2 md:h-full bg-white flex flex-col justify-center items-center p-4">
+        <h1 className="text-3xl font-bold mb-4">Login</h1>
+        <form className="w-full max-w-sm">
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
+              Username
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="username"
+              type="text"
+              placeholder="Username"
+            />
+          </div>
+          <div className="mb-6">
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+              Password
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+              id="password"
+              type="password"
+              placeholder="******************"
+            />
+          </div>
+          <div className="flex items-center justify-between">
+            <button
+              className="bg-blue-500 hover:bg-blue-900 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              type="button"
+            >
+              Sign In
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
-    )
-}
+  );
+};
 
-export default Login;
+export default LoginPage;
