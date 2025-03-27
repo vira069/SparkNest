e17a86332b858a6cc35a1b4994f93161873590d9
vira069 <vira.kumar069@gmail.com>
2025-03-27 15:20:35 +0530

    Update Tailwind CSS dependencies and enhance LoginPage styling

--- frontend/src/components/Auth/LoginPage.jsx
+++ frontend/src/components/Auth/LoginPage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { FaUser, FaLock, FaGithub, FaLinkedin, FaFacebook, FaGoogle } from "react-icons/fa"; // Importing icons
+//import loginImage from "../../assets/temp/LoginImage.jpeg";
 
 const LoginPage = () => {
   const handleSubmit = (event) => {
@@ -18,17 +19,17 @@ const LoginPage = () => {
   return (
     <div className="flex flex-col md:flex-row h-screen">
       {/* Left Section */}
-      <div className="w-full md:w-1/2 h-1/2 md:h-full bg-white flex justify-center items-center">
+      <div className="w-full md:w-1/2 h-1/2 bg-gradient-to-r from-blue-200 to-purple-100  md:h-full flex justify-center items-center">
         <img
-          src="https://plus.unsplash.com/premium_photo-1668736594225-55e292fdd95e?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+         // src={loginImage}
           alt="Login Illustration"
-          className="w-full h-full object-cover"
+          className="w-1/2 h-1/2"
         />
       </div>
 
       {/* Right Section */}
-      <div className="w-full md:w-1/2 h-1/2 md:h-full bg-gray-100 flex flex-col justify-center items-center p-4">
-        <h1 className="text-2xl font-bold mb-4 text-center">Login</h1>
+      <div className="w-full md:w-1/2 h-1/2 md:h-full bg-gradient-to-r from-blue-300 to-purple-200 flex flex-col justify-center items-center p-4">
+        <h1 className="text-2xl font-bold mb-4 text-center">LOGIN</h1>
         <form className="w-full max-w-sm" onSubmit={handleSubmit}>
           {/* Username Input with Icon */}
           <label
@@ -40,7 +41,7 @@ const LoginPage = () => {
           <div className="mb-3 relative flex items-center">
             <FaUser className="absolute left-2 text-gray-500" /> {/* Username Icon */}
             <input
-              className="shadow appearance-none border rounded w-full py-1.5 px-3 pl-8 text-sm text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              className="shadow appearance-none border rounded-2xl w-full py-1.5 px-3 pl-8 text-sm text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
               id="username"
               type="text"
               placeholder="Username"
@@ -57,7 +58,7 @@ const LoginPage = () => {
           <div className="mb-3 relative flex items-center">
             <FaLock className="absolute left-2 text-gray-500" /> {/* Password Icon */}
             <input
-              className="shadow appearance-none border rounded w-full py-1.5 px-3 pl-8 text-sm text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              className="shadow appearance-none border rounded-2xl w-full py-1.5 px-3 pl-8 text-sm text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
               id="password"
               type="password"
               placeholder="Password"
@@ -70,7 +71,7 @@ const LoginPage = () => {
                 type="checkbox"
                 className="form-checkbox accent-blue-500 focus:ring-blue-500"
               />
-              <span className="ml-1 text-gray-700">Remember Me</span>
+              <span className="ml-1 text-gray-700 text-xs">Remember Me</span>
             </label>
             <a
               href="#"
@@ -82,7 +83,7 @@ const LoginPage = () => {
 
           <div className="mb-4">
             <button
-              className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-1.5 px-3 rounded text-sm focus:outline-none focus:shadow-outline"
+              className="w-full rounded-2xl bg-blue-500 hover:bg-blue-700 text-white font-bold py-1.5 px-3 rounded text-sm focus:outline-none focus:shadow-outline"
               type="submit"
             >
               Sign In
@@ -113,16 +114,16 @@ const LoginPage = () => {
         <div className="mt-4 w-full max-w-sm">
         <p className="text-gray-700 text-xs text-center mb-3">--- or ---</p>
           <div className="grid grid-cols-2 gap-3">
-            <button className="bg-red-500 hover:bg-red-700 text-white py-1 px-2 rounded text-xs focus:outline-none focus:shadow-outline">
+            <button className="bg-red-500 hover:bg-red-700 text-white py-1 px-2 rounded-2xl text-xs focus:outline-none focus:shadow-outline">
             <FaGoogle className="inline mr-2" /> Google
             </button>
-            <button className="bg-blue-800 hover:bg-blue-600 text-white py-1 px-2 rounded text-xs focus:outline-none focus:shadow-outline">
+            <button className="bg-blue-800 hover:bg-blue-600 text-white py-1 px-2 rounded-2xl text-xs focus:outline-none focus:shadow-outline">
             <FaFacebook className="inline mr-2" /> Facebook
             </button>
-            <button className="bg-blue-600 hover:bg-blue-700 text-white py-1 px-2 rounded text-xs focus:outline-none focus:shadow-outline">
+            <button className="bg-blue-600 hover:bg-blue-700 text-white py-1 px-2 rounded-2xl text-xs focus:outline-none focus:shadow-outline">
             <FaLinkedin className="inline mr-2" /> LinkedIn
             </button>
-            <button className="bg-gray-700 hover:bg-gray-900 text-white py-1 px-2 rounded text-xs focus:outline-none focus:shadow-outline">
+            <button className="bg-gray-700 hover:bg-gray-900 text-white py-1 px-2 rounded-2xl text-xs focus:outline-none focus:shadow-outline">
             <FaGithub className="inline mr-2" /> GitHub
             </button>
           </div>
